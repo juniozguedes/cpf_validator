a7563b00c284f8f1522dfbe1b0cddff6370c0e5e
Wellington Guedes <juniozguedes@gmail.com>
2021-04-15 02:47:00 -0300

    Updated routes/views, D> validator, added LINTER

--- cpf_validator/cpf/urls.py
+++ cpf_validator/cpf/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/denied', views.DeniedCpfViewSet.as_view(), name='denied'),
-    path('details', views.CpfDetail.as_view(), name='retrieve_delete')
+    path('list', views.List.as_view(), name='list'),
+    path('denied', views.Denied.as_view(), name='denied'),
+    path('details', views.Detail.as_view(), name='details'),
+    path('create', views.Create.as_view(), name='create'),
 ]
