1ea1c974f7893d1235b456c3ddec99ad87e42d07
Wellington Guedes <juniozguedes@gmail.com>
2021-04-17 14:45:16 -0300

    Removed unused setting, finished README

--- cpf_validator/cpf_validator/urls.py
+++ cpf_validator/cpf_validator/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('cpf/', include('cpf.urls')),
-    path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='login'),
     path('api/token/refresh', TokenRefreshView.as_view()),
